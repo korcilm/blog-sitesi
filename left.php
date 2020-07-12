ace9f49c156e96afeee18a3685c13d37492bd5cc
korcilm <58364191+korcilm@users.noreply.github.com>
2020-07-12 21:55:15 +0300

    Add files via upload

---
+++ left.php
@@ -0,0 +1,38 @@
+<div class="col-md-3 mt-5" >
+
+  <div class="about-fixed">
+
+    <div class="my-pic">
+
+      <img src="images/fotom.jpg" style="width: 280px; " alt="">
+
+      <a href="javascript:void(0)" class="collapsed" data-target="#menu" data-toggle="collapse"><i class="icon-menu menu"></i></a>
+      <div id="menu" class="collapse">
+        <ul class="menu-link">
+          <li><a href="about.html">About</a></li>
+          <li><a href="work.html">Work</a></li>
+          <li><a href="contact.html">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+
+
+    <div class="my-detail">
+
+      <div class="white-spacing">
+        <h1>Muhammet Korçil</h1>
+        <span> Karabük Üniversitesi </span><br>
+        <span>Bilgisayar Mühendisliği</span>
+      </div>
+
+      <ul class="social-icon">
+        <li><a href="#" target="_blank" class="facebook"><i class="fa fa-envelope"></i></a></li>
+        <li><a href="#" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a></li>
+        <li><a href="#" target="_blank" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
+        <li><a href="#" target="_blank" class="github"><i class="fa fa-github"></i></a></li>
+      </ul>
+
+    </div>
+  </div>
+
+</div>
---
+++ sag.php
@@ -0,0 +1,37 @@
+<div class="col-md-3 mt-5">
+
+  <div class="card w-75 mx-5">
+    <div class="card-header">
+      <h4>Son Yazılar</h4>
+    </div>
+
+    <div class="card-body ">
+      <div class="row">
+        <div class="col">
+          <ul class="list-unstyled">
+            <li><a href="#">Web Design</a></li>
+            <li><a href="#">Html</a></li>
+            <li><a href="#">Javascript</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <ul class="list-unstyled">
+            <li><a href="#">Web Design</a></li>
+            <li><a href="#">Html</a></li>
+            <li><a href="#">Javascript</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="card w-75 mx-5 mt-3">
+    <div class="card-header">
+      <h4>Widget</h4>
+    </div>
+    <div class="card-body ">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+</div>
