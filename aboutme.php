d227f42cb07f8851baef2da3ee1f20b39c354d84
korcilm <58364191+korcilm@users.noreply.github.com>
2020-07-12 21:56:10 +0300

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,41 @@
+
+<?php include"header.php" ?> 
+
+<div class="container mb-5">
+        <div class="row ">
+            <div class="col-md-12">
+                <div class="card mb-3" style="margin-top:100px;">
+                    <div class="card-header">
+                        <div class="row ">
+                            <div class="col-md-6">
+                                <div class=" mt-1 ml-4 " style="width: 400px;">
+                                    <img src="images\fotom.jpg" class="card-img-top rounded-circle" alt="...">
+                                </div>
+
+                            </div>
+                            <div class="col-md-6">
+                                <div class="card bg-light mr-5" style="margin-top: 120px;">
+                                    <div class="card-body text-center">
+                                        <h1 class="card-title mb-3">Muhammet Korçil</h1>
+                                        <h5 class="card-title mb-3 text-secondary">Karabük Üniversitesi - Bilgisayar Mühendisliği</h5>
+
+                                        <a href="#"><i class="fab fa-linkedin fa-lg stack"></i></a>
+                                        <a href="#"><i class="fab fa-github-square fa-lg stack"></i></a>
+                                        <a href="#"><i class="fab fa-twitter-square fa-lg stack"></i></a>
+                                        <a href=""><i class="fas fa-envelope-square fa-lg stack"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title text-center">Konu Başlığı</h5>
+                        <p class="card-text text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ad repellendus, debitis ipsum deserunt aspernatur incidunt, natus officiis, quibusdam pariatur reprehenderit inventore, qui consequuntur sunt harum aut error dolorem vitae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ad repellendus, debitis ipsum deserunt aspernatur incidunt, natus officiis, quibusdam pariatur reprehenderit inventore, qui consequuntur sunt harum aut error dolorem vitae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ad repellendus, debitis ipsum deserunt aspernatur incidunt, natus officiis, quibusdam pariatur reprehenderit inventore, qui consequuntur sunt harum aut error dolorem vitae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ad repellendus, debitis ipsum deserunt aspernatur incidunt, natus officiis, quibusdam pariatur reprehenderit inventore, qui consequuntur sunt harum aut error dolorem vitae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ad repellendus, debitis ipsum deserunt aspernatur incidunt, natus officipariatur reprehenderit inventore, qui consequuntur sunt harum aut error dolorem vitae.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ad repellendus, debitis ipsum deserunt aspernatur incidunt, natus officiis, quibusdam pariatur reprehenderit inventore, qui consequuntur sunt harum aut error dolorem vitae. </p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include"footer.php" ?>
