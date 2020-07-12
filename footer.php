ae5b8c5b8f0c6943c6f304af60c8692e9eac0b43
korcilm <58364191+korcilm@users.noreply.github.com>
2020-07-12 21:51:11 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,17 +1,10 @@
-<!--footer-->
-<!-- End Grid -->
-</div>
-
-<!-- End Page Container -->
-</div>
-
-<footer >
-    <p> <a href="about.php" target="_blank">2020 - Muhammet Korçil</a></p>
-  </footer>
+<div id="footer" class="py-5 bg-dark text-white text-center mt-5"><a href="about.php" target="_blank">Copyright 2020 - Muhammet Korçil </a></div>
+<!-- All Javascript Plugins  -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/plugin.js"></script>
+<!-- Main Javascript File  -->
+<script type="text/javascript" src="js/scripts.js"></script>
+<script src="https://kit.fontawesome.com/26782133be.js"></script>
 
 </body>
 </html>
-
-
-
-
