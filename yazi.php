e0062853856ab6fb5c8936b9c62fbd144334e679
korcilm <58364191+korcilm@users.noreply.github.com>
2020-07-12 21:53:39 +0300

    Update yazi.php

--- yazi.php
+++ yazi.php
@@ -31,66 +31,88 @@ $query->execute(array($yazi_id));
 $cek=$query->fetch(PDO::FETCH_ASSOC);
 ?>	
 
-<div class="container">
-	<div class="blog">
-		<div class="blog-photo2"><img src="<?php echo $cek["yazi_link"]; ?>"></div>
-		<h2 class="blog-title"><?php echo $cek["yazi_adi"]; ?></h2>
-		<div class="blog-text">
-			<?php echo $cek["yazi_aciklama"]; ?>
-		</div>
-		<div class="blog-date"><?php echo $cek["yazi_tarih"]; ?></div>
-	</div>
-</div>
+<div class="ml-5 mr-5 mt-5">
+	<div class="row">
+		<?php include 'left.php'; ?>
 
-<div class="comment-respond">
-	<h3 class="comment-title">Bir cevap yazın </h3>	<hr>
-	<form action="admin\islem.php" method="POST" class="comment-form" id="form">
-		<p class="comment-notes">
-			<label for="form-notes">Yorumlarınız isminizle yayımlanır.</label>	
-		</p>
-		<p class="comment-form-comment">
-			<textarea id="comment" name="yorum" cols="45" rows="8" maxlength="65525" required="required" placeholder="Yorum yapınız.."></textarea>
-		</p>
-		<div class="comment-form-author">
-			<label for="author">İsim </label><br>
-			<input id="author" name="yorum_adi" type="text"  size="30" maxlength="245" required='required' />
-		</div>
-		<div class="comment-form-email">
-			<label for="email">E-posta </label><br>
-			<input id="email" name="yorum_mail" type="text"  size="30" maxlength="100" required='required' /> 
-			<input type="hidden" name="yazi_id" value="<?php echo $cek["id"]; ?>">
-		</div>
-		<div class="form-submit">
-			<input id="submit" name="yorum-ekle" type="submit" class="submit" value="Yorum Yap">
-		</div>
+		<div class="col-md-6 ">
 
-	</form>
-	<hr>
-</div>
-
-
-<?php 
-$yorumlar=$db->prepare("SELECT * FROM yorumlar WHERE yazi_id=$yazi_id2 and onay=1 ORDER BY yorum_id DESC ");
-$yorumlar->execute();
-$yorumcek=$yorumlar->fetchALL(PDO::FETCH_ASSOC);
-
-foreach ($yorumcek as $row ) {
-	?>	
-
-	<div class="contain">
-		<div class="yorum">
-			<h4 class="yorum-isim"><?php echo $row["yorum_adi"]; ?></h4>
-			<div class="yorum-text">
-				<?php echo $row["yorum"]; ?>
+			<div class="card mb-3" >
+				<img src="<?php echo $cek["yazi_link"]; ?>" class="card-img-top img-thumbnail" alt="...">
+				<div class="card-body">
+					<h5 class="card-title"><?php echo $cek["yazi_adi"]; ?></h5>
+					<p class="card-text"><?php echo $cek["yazi_aciklama"]; ?></p>
+				</div>
+				<div class="card-footer text-muted">
+					<div class="float-right"><?php echo $cek["yazi_tarih"]; ?></div>
+				</div>
 			</div>
-			<div class="yorum-date"><a href="#" class="yanit">Yanıtla</a> <?php echo $row["yorum_tarih"]; ?></div>
+
+			<div class="comment-respond">
+				<h3 class="comment-title">Bir cevap yazın </h3>	<hr>
+				<form action="admin\islem.php" method="POST" class="comment-form" id="form">
+					<p class="comment-notes">
+						<label for="form-notes">Yorumlarınız isminizle yayımlanır.</label>	
+					</p>
+					
+					<div class="form-group">
+						<textarea class="form-control" id="comment" name="yorum" rows="3" placeholder="Yorum yapınız.." required="required"></textarea>
+					</div>
+					
+					<div class="row">
+						<div class="form-group col-md-6">
+							<label for="author">İsim</label> 
+							<input type="text"  name="yorum_adi" class="form-control" id="author" required='required'>
+						</div>
+						<div class="form-group col-md-6">
+							<label for="email">Email</label>
+							<input type="email" name="yorum_mail" class="form-control" id="email" aria-describedby="emailHelp">
+							<input type="hidden" name="yazi_id" value="<?php echo $cek["id"]; ?>">
+							<small id="emailHelp" class="form-text text-muted" required='required'>Email adresiniz kimseyle paylaşılmamaktadır.</small>
+							
+						</div>
+					</div>
+
+					<div class="form-submit">
+						<input class="btn btn-primary" id="submit" name="yorum-ekle" type="submit" class="submit" value="Yorum Yap">
+					</div>
+
+				</form>
+				<hr>
+			</div>
+
+
+			<?php 
+			$yorumlar=$db->prepare("SELECT * FROM yorumlar WHERE yazi_id=$yazi_id2 and onay=1 ORDER BY yorum_id DESC ");
+			$yorumlar->execute();
+			$yorumcek=$yorumlar->fetchALL(PDO::FETCH_ASSOC);
+
+			foreach ($yorumcek as $row ) {
+				?>	
+				<ul class="list-unstyled">
+					<div class="card mb-2">
+						<li class="media">
+							<div class="m-3">
+								<i class="fas fa-user-circle fa-2x"></i>
+							</div>
+							
+							<div class="media-body mt-3 mr-4 text-justify">
+								<h5 class="mt-0 mb-1"><?php echo $row["yorum_adi"]; ?></h5>
+								<?php echo $row["yorum"]; ?>
+							</div>
+						</li>
+						<div class="card-footer text-muted">
+							<div class="float-right"><?php echo $row["yorum_tarih"]; ?></div>
+						</div>
+					</div>
+				</ul>
+
+				<?php 
+			}
+			?>
 		</div>
+
+		<?php include 'sag.php'; ?>
 	</div>
-	<?php 
-}
-?>
-
+</div>
 <?php include"footer.php" ?>
-
-</body>
-</html>
