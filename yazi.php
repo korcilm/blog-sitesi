151136fdc4ce5343a42ee3d2caf7e94127feb25c
korcilm <58364191+korcilm@users.noreply.github.com>
2020-08-08 19:49:34 +0300

    Update yazi.php

--- yazi.php
+++ yazi.php
@@ -1,116 +1,163 @@
 <?php include"header.php" ?>
-
+<div class="mt-0">
 <!-- alert uyarı bolmesi -->
 <?php 
 if (@$_GET["yorum-ekle"]=="bos") {
-	?>	
-	<div class=" alert-warning" >
-		<span > Boş alan bırakmayınız!</span>
-	</div>
+	?>
+	<div class="row justify-content-center">
+		<div class="alert alert-warning position-fixed alert-dismissible col-md-5 ">
+			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+			<i class="fas fa-exclamation-triangle"></i> Boş alan bırakmayınız!
+		</div>
+	</div>	
 	<?php
 }elseif (@$_GET["yorum-ekle"]=="yes") {
 	?>			
-	<div class="alert-success" >
-		<span > Yorumunuz başarıyla alınmıştır. Admin onayından sonra yayınlanacaktır.</span>
+	<div class="row justify-content-center  ">
+		<div class="alert alert-success position-fixed alert-dismissible col-md-5">
+			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+			<i class="fas fa-check-square"></i> Yorumunuz başarıyla alınmıştır. Admin onayından sonra yayınlanacaktır.
+		</div>
 	</div>
 	<?php
 }elseif (@$_GET["yorum-ekle"]=="no") {
 	?>
-	<div class=" alert-warning" >
-		<span > İşleminiz gerçekleşirken hata oluştu!</span>
+	<div class="row justify-content-center ">
+		<div class="alert alert-danger  position-fixed alert-dismissible col-md-5">
+			<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+			<i class="fas fa-exclamation-triangle"></i> İşleminiz gerçekleşirken hata oluştu!
+		</div>
 	</div>
 	<?php
 }
 ?>
-<?php 
-
-$yazi_id2=$yazi_id = @$_GET["id"];
-
-$query=$db->prepare("SELECT * FROM yazilarim WHERE id=?");
-$query->execute(array($yazi_id));
-$cek=$query->fetch(PDO::FETCH_ASSOC);
-?>	
+</div>	
 
 <div class="ml-5 mr-5 mt-5">
 	<div class="row">
 		<?php include 'left.php'; ?>
 
-		<div class="col-md-6 ">
+		<div class="col-md-6  mt-5">
 
-			<div class="card mb-3" >
-				<img src="<?php echo $cek["yazi_link"]; ?>" class="card-img-top img-thumbnail" alt="...">
+			<div class="card mb-3">	<?php 
+			
+			$yazi_id=$_GET["ID"];
+			$blog=$VT->VeriGetir("bloglar","WHERE ID=?",array($_GET["ID"]),"");
+			
+
+			if ($blog!=false) {
+
+				if (!empty($blog[0]["resim"])){
+					?>
+					<div class="card-header">
+						<img src="images/bloglar/<?=$blog[0]["resim"]?>" class="card-img-top" alt="..." style="max-height: 250px; ">
+					</div>
+					<?php
+				}
+				else{
+					?>
+					<div class="card-header">
+					</div>
+					<?php
+				}
+				?>
 				<div class="card-body">
-					<h5 class="card-title"><?php echo $cek["yazi_adi"]; ?></h5>
-					<p class="card-text"><?php echo $cek["yazi_aciklama"]; ?></p>
+					<h5 class="card-title"><?=$blog[0]["baslik"]?></h5>
+					<p class="card-text">
+						<?php
+						$metin=stripslashes($blog[0]["metin"]);
+						echo $metin;
+						?></p>
+					</div>
+
+					<div class="card-footer text-muted">
+
+						<div class="float-right">
+							<?php 
+
+							$date = $blog[0]["tarih"];
+							$date = date('d.m.Y  H:i', strtotime($date)); 
+							echo $date;
+						}
+						?>
+					</div> 
+
 				</div>
-				<div class="card-footer text-muted">
-					<div class="float-right"><?php echo $cek["yazi_tarih"]; ?></div>
-				</div>
-			</div>
+			</div>	
 
 			<div class="comment-respond">
 				<h3 class="comment-title">Bir cevap yazın </h3>	<hr>
-				<form action="admin\islem.php" method="POST" class="comment-form" id="form">
+				<form action="panel\islem.php" method="POST" class="comment-form" id="form">
 					<p class="comment-notes">
 						<label for="form-notes">Yorumlarınız isminizle yayımlanır.</label>	
 					</p>
 					
 					<div class="form-group">
-						<textarea class="form-control" id="comment" name="yorum" rows="3" placeholder="Yorum yapınız.." required="required"></textarea>
+						<textarea class="form-control" id="comment" name="yorum" rows="3" placeholder="Yorum yapınız.." ></textarea>
 					</div>
 					
 					<div class="row">
 						<div class="form-group col-md-6">
 							<label for="author">İsim</label> 
-							<input type="text"  name="yorum_adi" class="form-control" id="author" required='required'>
+							<input type="text"  name="kullaniciadi" class="form-control" id="author">
 						</div>
 						<div class="form-group col-md-6">
 							<label for="email">Email</label>
-							<input type="email" name="yorum_mail" class="form-control" id="email" aria-describedby="emailHelp">
-							<input type="hidden" name="yazi_id" value="<?php echo $cek["id"]; ?>">
-							<small id="emailHelp" class="form-text text-muted" required='required'>Email adresiniz kimseyle paylaşılmamaktadır.</small>
+							<input type="email" name="mail" class="form-control" id="email" aria-describedby="emailHelp">
+							<input type="hidden" name="yazi_id" value="<?=$_GET["ID"]?>">
+							<small id="emailHelp" class="form-text text-muted">Email adresiniz kimseyle paylaşılmamaktadır.</small>
 							
 						</div>
 					</div>
 
 					<div class="form-submit">
-						<input class="btn btn-primary" id="submit" name="yorum-ekle" type="submit" class="submit" value="Yorum Yap">
+						<input class="btn btn-primary" id="submit" name="yorum-ekle" type="submit" value="Yorum Yap">
 					</div>
 
 				</form>
 				<hr>
-			</div>
-
+			</div>	
 
 			<?php 
-			$yorumlar=$db->prepare("SELECT * FROM yorumlar WHERE yazi_id=$yazi_id2 and onay=1 ORDER BY yorum_id DESC ");
-			$yorumlar->execute();
-			$yorumcek=$yorumlar->fetchALL(PDO::FETCH_ASSOC);
+			$yorum=$VT->VeriGetir("yorumlar","WHERE blogID=? AND onay=?",array($yazi_id,1),"ORDER BY ID DESC");
+			if ($yorum!=false) {
 
-			foreach ($yorumcek as $row ) {
-				?>	
-				<ul class="list-unstyled">
-					<div class="card mb-2">
-						<li class="media">
-							<div class="m-3">
-								<i class="fas fa-user-circle fa-2x"></i>
+				for ($i=0; $i < count($yorum) ; $i++) { 
+
+
+					?>	
+					<ul class="list-unstyled">
+						<div class="card mb-2">
+							<li class="media">
+								<div class="m-3">
+									<i class="fas fa-user-circle fa-2x"></i>
+								</div>
+
+								<div class="media-body mt-3 mr-4 text-justify">
+									<h5 class="mt-0 mb-1"><?=$yorum[$i]["kullaniciadi"]?></h5>
+									<?=$yorum[$i]["yorum"]?>
+								</div>
+							</li>
+							<div class="card-footer text-muted">
+								<div class="float-right"><?php 
+
+								$date = $yorum[$i]["tarih"];
+								$date = date('d.m.Y  H:i', strtotime($date)); 
+								echo $date;
+
+								?></div>
 							</div>
-							
-							<div class="media-body mt-3 mr-4 text-justify">
-								<h5 class="mt-0 mb-1"><?php echo $row["yorum_adi"]; ?></h5>
-								<?php echo $row["yorum"]; ?>
-							</div>
-						</li>
-						<div class="card-footer text-muted">
-							<div class="float-right"><?php echo $row["yorum_tarih"]; ?></div>
 						</div>
-					</div>
-				</ul>
+					</ul>
 
-				<?php 
+					<?php 
+
+				}
 			}
 			?>
+			<hr>
 		</div>
+		
 
 		<?php include 'sag.php'; ?>
 	</div>
